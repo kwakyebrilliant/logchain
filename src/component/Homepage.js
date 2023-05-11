5c98e92a1c775c818c34694e3cd582b248525179
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-11 18:37:46 +0000

    home updates

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navigation from './Navigation'
 
+import { FaCartArrowDown } from 'react-icons/fa'
+
 import landing from '../assets/landing.svg';
 
 function Homepage() {
@@ -64,28 +66,11 @@ function Homepage() {
             >
               <div className="flex items-start gap-4">
                 <span className="shrink-0 rounded-lg bg-emerald-600 p-4">
-                  <svg
-                    className="h-5 w-5 text-white"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                    <path
-                      d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                    ></path>
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                    ></path>
-                  </svg>
+                <FaCartArrowDown className=' text-white lg:w-5 lg:h-5' />
                 </span>
 
                 <div>
-                  <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+                  <h2 className="text-lg font-bold">Order.</h2>
 
                   <p className="mt-1 text-sm text-gray-400">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
