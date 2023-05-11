66cd4a25015c656a8fb04871d41144208dc82c56
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-11 15:29:03 +0000

    home updates

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -8,34 +8,32 @@ function Homepage() {
       <Navigation />
 
       <section>
-  <div
-    className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
-  >
-    <div className="mx-auto text-center">
-      <h1
-        className="bg-gradient-to-r from-blue-500 via-green-400 to-emerald-700 bg-clip-text text-3xl font-extrabold text-transparent lg:text-7xl"
-      >
-        Combating Fake Drugs.
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto text-center">
+          <h1
+            className="bg-gradient-to-r from-blue-500 via-green-400 to-emerald-700 bg-clip-text text-3xl font-extrabold text-transparent lg:text-7xl"
+          >
+            Combating Fake Drugs.
 
-        <span className="sm:block"> Through Blockchain. </span>
-      </h1>
+            <span className="sm:block"> Through Blockchain. </span>
+          </h1>
 
-      <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-        tenetur fuga ducimus numquam ea!
-      </p>
+          <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+            tenetur fuga ducimus numquam ea!
+          </p>
 
-      <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <a
-          className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-          href="/get-started"
-        >
-          Read More
-        </a>
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+              href="/get-started"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
 
 
       <h1>Homepage</h1>
