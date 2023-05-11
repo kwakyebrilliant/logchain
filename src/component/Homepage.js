9d0987b83c8b6ba57f61e19ab548ee986d6ee56d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-11 18:50:00 +0000

    home updates

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navigation from './Navigation'
 
-import { FaCartArrowDown } from 'react-icons/fa'
+import { FaCartArrowDown, FaShippingFast, FaCheckCircle } from 'react-icons/fa'
 
 import landing from '../assets/landing.svg';
 
@@ -61,9 +61,8 @@ function Homepage() {
               </p>
             </div>
 
-            <div
-              className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3"
-            >
+            <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+
               <div className="flex items-start gap-4">
                 <span className="shrink-0 rounded-lg bg-emerald-600 p-4">
                 <FaCartArrowDown className=' text-white lg:w-5 lg:h-5' />
@@ -80,10 +79,39 @@ function Homepage() {
                 </div>
               </div>
 
-              
+
+              <div className="flex items-start gap-4">
+                <span className="shrink-0 rounded-lg bg-emerald-600 p-4">
+                <FaShippingFast className=' text-white lg:w-5 lg:h-5' />
+                </span>
+
+                <div>
+                  <h2 className="text-lg font-bold">Track.</h2>
+
+                  <p className="mt-1 text-sm text-gray-400">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                    cumque tempore est ab possimus quisquam reiciendis tempora animi!
+                    Quaerat, saepe?
+                  </p>
+                </div>
+              </div>
 
               
+              <div className="flex items-start gap-4">
+                <span className="shrink-0 rounded-lg bg-emerald-600 p-4">
+                <FaCheckCircle className=' text-white lg:w-5 lg:h-5' />
+                </span>
 
+                <div>
+                  <h2 className="text-lg font-bold">Receive.</h2>
+
+                  <p className="mt-1 text-sm text-gray-400">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                    cumque tempore est ab possimus quisquam reiciendis tempora animi!
+                    Quaerat, saepe?
+                  </p>
+                </div>
+              </div>
             
 
              
