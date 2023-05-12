a523d71ca5967c3386167230249c719c15e71561
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-12 11:07:11 +0000

    app updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from './component/Homepage';
+import Login from './component/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route path="/" exact element={<Homepage />} />
+      <Route path="/login" exact element={<Login />} />
       </Routes>
     </Router>
   )
