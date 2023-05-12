77990677ce9dfb61784882702cc554523d9603c5
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-12 13:02:51 +0000

    login updates

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,77 +1,105 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-
-import { FaHospitalSymbol } from 'react-icons/fa'
-import pharmacy from '../assets/pharmacy_c.jpg'
 
 function Login() {
   return (
     <div>
 
-                <section className="bg-white">
-                <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-                    <section
-                    className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
-                    >
-                    <img
-                        alt="pharmacy"
-                        src={pharmacy}
-                        className="absolute inset-0 h-full w-full object-cover opacity-80"
+            <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-lg">
+                <h1 class="text-center text-2xl font-bold text-emerald-600 sm:text-3xl">
+                Get started today
+                </h1>
+
+                <p class="mx-auto mt-4 max-w-md text-center text-gray-500">
+                    Using blockchain technology, we are Solving the supply chain problems in the pharmaceutical industry.
+                </p>
+
+                <form
+                action=""
+                class="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+                >
+                <p class="text-center text-lg font-medium">Choose your account</p>
+
+                <div>
+                    <label for="email" class="sr-only">Email</label>
+
+                    <div class="relative">
+                    <input
+                        type="email"
+                        class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                        placeholder="Enter email"
                     />
 
-                    <div className="hidden lg:relative lg:block lg:p-12">
-                        <a className="block text-white" href="/">
-                            <FaHospitalSymbol className=' text-white lg:w-[35px] lg:h-[35px]' />
-                        </a>
-
-                        <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-                        Welcome to Logchain
-                        </h2>
-
-                        <p className="mt-4 leading-relaxed text-white/90">
-                            Using blockchain technology, we are Solving the supply chain problems in the pharmaceutical industry.
-                        </p>
-                    </div>
-                    </section>
-
-                    <main
-                    aria-label="Main"
-                    className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
-                    >
-                    <div className="max-w-xl lg:max-w-3xl">
-                        <div className="relative -mt-16 block lg:hidden">
-                        <a
-                            className="inline-flex h-16 w-16 items-center justify-center rounded-full bg-white text-blue-600 sm:h-20 sm:w-20"
-                            href="/"
+                    <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-4 w-4 text-gray-400"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
                         >
-                           <FaHospitalSymbol className=' text-emerald-600 lh-8 sm:h-10' />
-                        </a>
-
-                        <h1
-                            className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
-                        >
-                            Welcome to Logchain
-                        </h1>
-
-                        <p className="mt-4 leading-relaxed text-gray-500">
-                        Using blockchain technology, we are Solving the supply chain problems in the pharmaceutical industry.
-                        </p>
-                        </div>
-
-                        <form action="#" className="mt-8 grid grid-cols-6 gap-6">
-                        
-
-                       
-
-
-                        
-                        </form>
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                        />
+                        </svg>
+                    </span>
                     </div>
-                    </main>
                 </div>
-                </section>
 
-       
+                <div>
+                    <label for="password" class="sr-only">Password</label>
+
+                    <div class="relative">
+                    <input
+                        type="password"
+                        class="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                        placeholder="Enter password"
+                    />
+
+                    <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-4 w-4 text-gray-400"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                        />
+                        </svg>
+                    </span>
+                    </div>
+                </div>
+
+                <button
+                    type="submit"
+                    class="block w-full rounded-lg bg-emerald-600 px-5 py-3 text-sm font-medium text-white"
+                >
+                    Sign in
+                </button>
+
+                <p class="text-center text-sm text-gray-500">
+                    No account?
+                    <a class="underline" href="/">Submit</a>
+                </p>
+                </form>
+            </div>
+            </div>
+
+
     </div>
   )
 }
