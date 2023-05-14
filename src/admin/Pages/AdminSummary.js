e1051398acf0be11df2588a3193cccc852885a10
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-14 11:46:28 +0000

    adminsummry updates

--- src/admin/Pages/AdminSummary.js
+++ src/admin/Pages/AdminSummary.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PartialNavbar from '../Partial/PartialNavbar'
 import Sidebar from '../Partial/Sidebar'
 
+import { FaPlusCircle } from "react-icons/fa";
+
 function AdminSummary() {
   return (
     <div className='text-black'>
@@ -31,6 +33,15 @@ function AdminSummary() {
             </div>
             </header>
             </div>
+
+            <div className='flex m-5'>
+            <button className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              <FaPlusCircle className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+              Add Product
+            </button>
+            </div>
+
+            
 
 
 
