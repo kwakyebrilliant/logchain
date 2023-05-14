adf7195d778874c0901209251b07761ab370f8da
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-14 17:35:52 +0000

    manager updates

--- src/admin/Pages/Manager.js
+++ src/admin/Pages/Manager.js
@@ -1,6 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import PartialNavbar from '../Partial/PartialNavbar'
 import Sidebar from '../Partial/Sidebar'
+
+import {
+    FaCheckCircle,
+    FaExclamation
+  } from "react-icons/fa";
 
 function Manager() {
   return (
@@ -12,7 +18,83 @@ function Manager() {
 
             <div className='grow'>
                 <PartialNavbar />
+
+                <div className='m-5'>
+            
+            <header className="rounded-2xl inset-x-0 bottom-0 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+            <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+                <div className="sm:justify-between sm:items-center sm:flex">
+                <div className="text-center sm:text-left">
+                    <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                    Welcome Back, Brilliant!
+                    </h1>
+
+                    <p className="mt-1.5 text-sm text-gray-900">
+                    Check your statistics on LOGCHAIN 🎉
+                    </p>
+                </div>
+
                 
+                </div>
+            </div>
+            </header>
+            </div>
+
+
+            <div className="m-5 grid mt-12 grid-cols-1 gap-8 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+
+            
+              
+            <a className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-md sm:p-6 lg:p-8">
+              <div className="pt-4 text-gray-500">
+
+              <span className="flex w-fit p-4 rounded-lg bg-emerald-600">
+              <FaExclamation className=' text-white lg:w-5 lg:h-5' />
+                </span>
+
+                <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+                All Pending Orders
+                </h3>
+
+                <p className="mt-2 hidden text-sm sm:block">
+                  TOTAL NUMBER OF PENDING ORDERS
+                </p>
+              </div>
+
+              <span
+                className="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+              >
+                0
+              </span>
+            </a>
+
+
+            <a className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-md sm:p-6 lg:p-8">
+              <div className="pt-4 text-gray-500">
+
+              <span className="flex w-fit p-4 rounded-lg bg-emerald-600">
+              <FaCheckCircle className=' text-white lg:w-5 lg:h-5' />
+                </span>
+
+                <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+                  All Confirmed Orders
+                </h3>
+
+                <p className="mt-2 hidden text-sm sm:block">
+                TOTAL NUMBER OF CONFIRMED ORDERS
+                </p>
+              </div>
+
+              <span
+                className="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+              >
+                0
+              </span>
+            </a>
+
+
+             
+            </div>
 
 
             </div>
