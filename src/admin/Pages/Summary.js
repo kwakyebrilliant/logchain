44c8bbd1432b1388819cd168ad470a0966619ce1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-14 11:21:55 +0000

    admin summary added

---
+++ src/admin/Pages/Summary.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PartialNavbar from "../Partials/PartialNavbar";
+import Sidebar from '../Partials/Sidebar';
+
+function Summary() {
+  return (
+    <div className='text-black'>
+      <>
+        <div className='flex flex-auto'>
+          <Sidebar />
+          <div className='grow'>
+            <PartialNavbar />
+
+            <div className='m-5'>
+            
+            <header className="rounded-2xl inset-x-0 bottom-0 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
+            <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+                <div className="sm:justify-between sm:items-center sm:flex">
+                <div className="text-center sm:text-left">
+                    <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                    Welcome Back, Brilliant!
+                    </h1>
+
+                    <p className="mt-1.5 text-sm text-gray-900">
+                    Check your statistics on LOGCHAIN 🎉
+                    </p>
+                </div>
+
+                
+                </div>
+            </div>
+            </header>
+            </div>
+
+
+
+          </div>
+        </div>
+      </>
+    </div>
+  )
+}
+
+export default Summary
