14e551cd84a16ff64a64d84ba1ca9072499d016b
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-14 17:30:35 +0000

    manager updates

--- src/admin/Pages/Manager.js
+++ src/admin/Pages/Manager.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import PartialNavbar from '../Partial/PartialNavbar'
+import Sidebar from '../Partial/Sidebar'
 
 function Manager() {
   return (
-    <div>Manager</div>
+    <div className='text-black'>
+        <>
+        
+        <div className='flex flex-auto'>
+            <Sidebar />
+
+            <div className='grow'>
+                <PartialNavbar />
+                
+
+
+            </div>
+
+        </div>
+        
+        </>
+    </div>
   )
 }
 
