abd4252e7efc0636d4234101aebb248218e29c8e
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-26 23:20:21 +0000

    tokenization updates

--- src/component/Tokenization.js
+++ src/component/Tokenization.js
@@ -9,93 +9,82 @@ function Tokenization() {
 
       <Navigation />
 
-        <div className="relative pt-8">
-          <img src={images2} alt="Background" className="w-full rounded object-cover h-screen" />
-    
-          <div
-      className="grid absolute inset-0 pt-40 p-8 grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16"
-    >
-      <div
-        className="mx-auto max-w-lg lg:mx-0 ltr:lg:text-left rtl:lg:text-right"
-      >
-        <h2 className="text-3xl font-bold sm:text-4xl">Find tokenized assets</h2>
+          <div className="relative pt-8">
+            <img src={images2} alt="images2" className="w-full rounded object-cover h-screen" />
+      
+            <div className="grid absolute inset-0 pt-40 p-8 grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
+              <div className="mx-auto max-w-lg lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
+                <h2 className="text-3xl font-bold sm:text-4xl">Find Tokenized Assets</h2>
 
-        <p className="mt-4 text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero
-          aliquid sint distinctio iure ipsum cupiditate? Quis, odit assumenda?
-          Deleniti quasi inventore, libero reiciendis minima aliquid tempora.
-          Obcaecati, autem.
-        </p>
+                <p className="mt-4 text-gray-600">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero
+                  aliquid sint distinctio iure ipsum cupiditate? Quis, odit assumenda?
+                  Deleniti quasi inventore, libero reiciendis minima aliquid tempora.
+                  Obcaecati, autem.
+                </p>
 
-        <a
-          className="mt-8 inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-emerald-600 hover:border-emerald-600 hover:border cursor-pointer focus:outline-none focus:ring"
-        >
-          Get Started
-        </a>
-      </div>
+                <a className="mt-8 inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-emerald-600 hover:border-emerald-600 hover:border cursor-pointer focus:outline-none focus:ring">
+                  Find Them
+                </a>
+              </div>
 
-      <div>
-      <h2 className="text-2xl font-bold mb-4 justify-center flex sm:text-3xl">Tokenization Statistics</h2>
+              <div>
+              <h2 className="text-2xl font-bold mb-4 justify-center flex sm:text-3xl">Tokenization Statistics</h2>
 
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-2">
-        
+              <div className="grid grid-cols-2 gap-4 sm:grid-cols-2">
+                
 
-      <div
-          className="flex flex-col rounded-lg border border-emerald-600 hover:bg-gray-200 hover:border-gray-200 px-4 py-8 text-center"
-        >
-          <dt className="order-last text-lg font-medium text-gray-500">
-            Outstanding Invoices
-          </dt>
+              <div className="flex flex-col rounded-lg border border-emerald-600 hover:bg-gray-200 hover:border-gray-200 px-4 py-8 text-center">
+                  <dt className="order-last text-lg font-medium text-gray-500">
+                    Outstanding Invoices
+                  </dt>
 
-          <dd className="text-4xl font-extrabold mb-4 text-emerald-600 md:text-5xl">
-            4.8m
-          </dd>
-        </div>
+                  <dd className="text-4xl font-extrabold mb-4 text-emerald-600 md:text-5xl">
+                    4.8m
+                  </dd>
+                </div>
 
-        <div
-          className="flex flex-col rounded-lg border border-emerald-600 hover:bg-gray-200 hover:border-gray-200 px-4 py-8 text-center"
-        >
-          <dt className="order-last text-lg font-medium text-gray-500">
-            Undue Invoices
-          </dt>
+                <div className="flex flex-col rounded-lg border border-emerald-600 hover:bg-gray-200 hover:border-gray-200 px-4 py-8 text-center">
+                  <dt className="order-last text-lg font-medium text-gray-500">
+                    Undue Invoices
+                  </dt>
 
-          <dd className="text-4xl font-extrabold mb-4 text-emerald-600 md:text-5xl">
-            4.8m
-          </dd>
-        </div>
+                  <dd className="text-4xl font-extrabold mb-4 text-emerald-600 md:text-5xl">
+                    4.8m
+                  </dd>
+                </div>
 
 
-        <div
-          className="flex flex-col rounded-lg border border-emerald-600 hover:bg-gray-200 hover:border-gray-200 px-4 py-8 text-center"
-        >
-          <dt className="order-last text-lg font-medium text-gray-500">
-            Insurance Coverage
-          </dt>
+                <div className="flex flex-col rounded-lg border border-emerald-600 hover:bg-gray-200 hover:border-gray-200 px-4 py-8 text-center">
+                  <dt className="order-last text-lg font-medium text-gray-500">
+                    Insurance Coverage
+                  </dt>
 
-          <dd className="text-4xl font-extrabold mb-4 text-emerald-600 md:text-5xl">
-            4.8m
-          </dd>
-        </div>
+                  <dd className="text-4xl font-extrabold mb-4 text-emerald-600 md:text-5xl">
+                    4.8m
+                  </dd>
+                </div>
 
-        <div
-          className="flex flex-col rounded-lg border border-emerald-600 hover:bg-gray-200 hover:border-gray-200 px-4 py-8 text-center"
-        >
-          <dt className="order-last text-lg font-medium text-gray-500">
-            Tokenization Value
-          </dt>
+                <div className="flex flex-col rounded-lg border border-emerald-600 hover:bg-gray-200 hover:border-gray-200 px-4 py-8 text-center">
+                  <dt className="order-last text-lg font-medium text-gray-500">
+                    Tokenization Value
+                  </dt>
 
-          <dd className="text-4xl font-extrabold mb-4 text-emerald-600 md:text-5xl">
-            $4.8m
-          </dd>
-        </div>
-       
+                  <dd className="text-4xl font-extrabold mb-4 text-emerald-600 md:text-5xl">
+                    $4.8m
+                  </dd>
+                </div>
+              
 
 
-      </div>
-      </div>
-    </div>
-                    
-        </div>
+              </div>
+              </div>
+            </div>
+                      
+          </div>
+
+
+          
 
       
     </div>
