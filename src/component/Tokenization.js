c1eb9d8479b360dc8d3f89dfb6cab02c600e0e9c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-26 00:06:27 +0000

    tokenization updates

--- src/component/Tokenization.js
+++ src/component/Tokenization.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 function Tokenization() {
   return (
-    <div>Tokenization</div>
+    <div>
+
+      <Navigation />
+
+      <h1 className='pt-20'>
+      Tokenization
+      </h1>
+    </div>
   )
 }
 
