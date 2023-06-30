aaa9610481e6a991a16cd0d3ad88042eb686c754
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-30 21:30:13 +0000

    ifactoring updates

--- src/component/IFactoring.js
+++ src/component/IFactoring.js
@@ -104,9 +104,7 @@ function IFactoring() {
               </span>
             </div>
 
-            </div>
-
-              
+            </div>      
               
             </div>
             
--- src/component/TFinancing.js
+++ src/component/TFinancing.js
@@ -25,6 +25,89 @@ function TFinancing() {
 
         <div className="relative z-0 py-8">
 
+        <nav aria-label="Breadcrumb" className='pt-12'>
+            <ol className="flex justify-start items-center gap-1 text-sm text-gray-600">
+                <li>
+                <span href="#" className="block transition hover:text-gray-700">
+                    <span className="sr-only"> Invoicing </span>
+
+                    <FaFileAlt className='lg:w-4 lg:h-4' />
+                </span>
+                </li>
+
+                <li className="rtl:rotate-180">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                >
+                    <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                    />
+                </svg>
+                </li>
+
+                <li>
+                <span className="block transition hover:text-gray-700"> Trade Financing </span>
+                </li>
+
+
+            </ol>
+            </nav>
+
+            <div className="grid container mx-auto pt-20 p-8 grid-cols-1 gap-y-8 lg:grid-cols-1 lg:gap-x-16">
+            <div className="mx-auto max-w-lg">
+              <h2 className="text-3xl font-bold sm:text-6xl">Trade Financing.</h2>
+              
+              <div className='border-2 mt-8 border-emerald-500 p-8 bg-transparent rounded-3xl'>
+              <p className="mt-4 text-gray-600">
+                Search invoices not due
+              </p>
+
+            <div className="relative mt-2 mb-12">
+              <label for="Search" className="sr-only"> Search </label>
+
+              <input
+                type="text"
+                id="Search"
+                placeholder="Search invoices..."
+                className="w-full h-12 focus:outline-none rounded-full border-none border-gray-200 pe-10 ps-4 text-sm  py-2.5 shadow-sm sm:text-sm"
+              />
+
+              <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+                <button 
+                  type="button" 
+                  className="absolute border border-emerald-700 end-1 top-1/2 -translate-y-1/2 rounded-full bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
+                  >
+                  <span className="sr-only">Search</span>
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-4 w-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                    />
+                  </svg>
+                </button>
+              </span>
+            </div>
+
+            </div>      
+              
+            </div>
+
+            </div>
+
 
         </div>
 
