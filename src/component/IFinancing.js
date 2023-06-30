f1852e1d7f1be9b029ac1c61489d2d98ac5c9579
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-06-30 12:59:33 +0000

    assets updates

--- src/component/IFinancing.js
+++ src/component/IFinancing.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navigation from './Navigation';
 
+import {
+    FaFileAlt,
+  } from 'react-icons/fa'
+
 function IFinancing() {
   return (
     <div>
@@ -8,51 +12,38 @@ function IFinancing() {
          
          
 
-<nav aria-label="Breadcrumb" className='pt-20'>
-  <ol className="flex justify-start items-center gap-1 text-sm text-gray-600">
-    <li>
-      <span href="#" className="block transition hover:text-gray-700">
-        <span className="sr-only"> Invoicing </span>
+            <nav aria-label="Breadcrumb" className='pt-20'>
+            <ol className="flex justify-start items-center gap-1 text-sm text-gray-600">
+                <li>
+                <span href="#" className="block transition hover:text-gray-700">
+                    <span className="sr-only"> Invoicing </span>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-      </span>
-    </li>
+                    <FaFileAlt className='lg:w-4 lg:h-4' />
+                </span>
+                </li>
 
-    <li className="rtl:rotate-180">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-4 w-4"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </li>
+                <li className="rtl:rotate-180">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                >
+                    <path
+                    fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"
+                    />
+                </svg>
+                </li>
 
-    <li>
-      <span className="block transition hover:text-gray-700"> Invoice Financing </span>
-    </li>
+                <li>
+                <span className="block transition hover:text-gray-700"> Invoice Financing </span>
+                </li>
 
 
-  </ol>
-</nav>
+            </ol>
+            </nav>
         IFinancing
     </div>
   )
