7126ca0a448fa9ebeec69f5a1c067324f067ddbf
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-01 13:05:47 +0000

    tfinancing updates

--- src/component/TFinancing.js
+++ src/component/TFinancing.js
@@ -141,11 +141,13 @@ function TFinancing() {
                     <div class="grid p-6 bg-gray-100 rounded place-content-center sm:p-8">
                         <div class="max-w-md mx-auto text-center lg:text-left">
                         <header>
-                            <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">Watches</h2>
+                            <h2 class="text-xl font-bold text-gray-900 sm:text-3xl">Asset Brook</h2>
 
                             <p class="mt-4 text-gray-500">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
-                            rerum quam amet provident nulla error!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                            voluptatem alias ut provident sapiente repellendus.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                            voluptatem alias ut provident sapiente repellendus.
                             </p>
                         </header>
 
@@ -153,7 +155,7 @@ function TFinancing() {
                             href="#"
                             class="inline-block px-12 py-3 mt-8 text-sm font-medium text-white transition bg-gray-900 border border-gray-900 rounded hover:shadow focus:outline-none focus:ring"
                         >
-                            Shop All
+                            Make Offer
                         </a>
                         </div>
                     </div>
