55caf1af8ef646aa16609f4948f645def5d9aa1d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-03 23:31:51 +0000

    Tokenizationsingle updates

--- src/dashboard/Single/Tokenizationsingle.js
+++ src/dashboard/Single/Tokenizationsingle.js
@@ -1,8 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import { FaArrowCircleLeft } from 'react-icons/fa'
 
 function Tokenizationsingle() {
+
+    const handleGoBack = () => {
+        window.history.back();
+      };
+
   return (
-    <div>Tokenizationsingle</div>
+    <div className='m-8'>
+
+        <div>
+        <a 
+        onClick={handleGoBack}
+        className="inline-block cursor-pointer rounded-full border border-emerald-600 bg-emerald-600 p-3 text-white hover:bg-transparent hover:text-emerald-600 focus:outline-none focus:ring active:text-emerald-500">
+
+        <FaArrowCircleLeft className='lg:w-5 lg:h-5' />
+            </a>
+        </div>
+        
+        Tokenizationsingle
+    </div>
   )
 }
 
