362ab7c3b582fde8649bc82ea9f0e0c87ce3370c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-04 20:41:06 +0000

    supplier updates

--- src/dashboard/Pages/Supplier.js
+++ src/dashboard/Pages/Supplier.js
@@ -25,7 +25,7 @@ function Supplier() {
                 <div className="sm:justify-between sm:items-center sm:flex">
                 <div className="text-center sm:text-left">
                     <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
-                    Welcome Back, Buyer!
+                    Welcome Back, Supplier!
                     </h1>
 
                     <p className="mt-1.5 text-sm text-gray-900">
