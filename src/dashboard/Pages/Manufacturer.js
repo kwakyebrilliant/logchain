0cb9b47f1544d12a0fe412d55906947a339eb15e
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-04 14:32:01 +0000

    manufacturer updates

--- src/dashboard/Pages/Manufacturer.js
+++ src/dashboard/Pages/Manufacturer.js
@@ -26,11 +26,11 @@ function Manufacturer() {
                 <div className="sm:justify-between sm:items-center sm:flex">
                 <div className="text-center sm:text-left">
                     <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
-                    Welcome Back, Brilliant!
+                    Welcome Back, Manufacturer!
                     </h1>
 
                     <p className="mt-1.5 text-sm text-gray-900">
-                    Check your statistics on LOGCHAIN 🎉
+                    Check your statistics on XRChain 🎉
                     </p>
                 </div>
 
@@ -44,6 +44,29 @@ function Manufacturer() {
             <div className="m-5 grid mt-12 grid-cols-1 gap-8 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
             
               
+            <a className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-md sm:p-6 lg:p-8">
+              <div className="pt-4 text-gray-500">
+
+              <span className="flex w-fit p-4 rounded-lg bg-emerald-600">
+              <FaExclamation className=' text-white lg:w-5 lg:h-5' />
+                </span>
+
+                <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+                All Pending Orders
+                </h3>
+
+                <p className="mt-2 hidden text-sm sm:block">
+                  TOTAL NUMBER OF PENDING ORDERS
+                </p>
+              </div>
+
+              <span
+                className="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+              >
+                0
+              </span>
+            </a>
+
             <a className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-md sm:p-6 lg:p-8">
               <div className="pt-4 text-gray-500">
 
