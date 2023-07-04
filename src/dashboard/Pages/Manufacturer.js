b43f9b4891ea74df705eb3263b7ee4f6aeacb4bd
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-07-04 19:06:58 +0000

    manufacturer updates

--- src/dashboard/Pages/Manufacturer.js
+++ src/dashboard/Pages/Manufacturer.js
@@ -198,7 +198,28 @@ function Manufacturer() {
                 <td className="whitespace-nowrap px-4 py-2 text-lg text-center text-gray-700">24/05/1995</td>
                 <td className="whitespace-nowrap px-4 py-2 text-lg text-center text-gray-700">Web Developer</td>
                 <td className="whitespace-nowrap px-4 py-2 text-lg text-center text-gray-700">24/05/1995</td>
-                <td className="whitespace-nowrap px-4 py-2 text-lg text-center text-gray-700">Trade Finance</td>
+                <td className="whitespace-nowrap px-4 py-2 text-lg text-center text-gray-700">
+                <span
+                className="inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700"
+                >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="-ms-1 me-1.5 h-4 w-4"
+                >
+                    <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                </svg>
+
+                <p className="whitespace-nowrap text-sm">Tokenize</p>
+                </span>
+                </td>
                 <td className="whitespace-nowrap px-4 py-2 text-lg text-center text-gray-700">
                 <a 
                 href='/tokenizationsingle'
